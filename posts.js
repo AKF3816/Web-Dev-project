983025da8dab78a4bad237a95ed301af4bb462d4
AKF3816 <aa2308229@qu.edu.qa>
2026-03-19 06:36:27 +0300

    main feed
    added post display/rendering function, fixed error in stylesheet link, added more css styling

--- posts.js
+++ posts.js
@@ -1,1 +1,21 @@
-
+const feed = document.getElementById("posts")
+const posts =[]
+function displayPosts(){
+    feed.innerHTML="";
+    posts.forEach(posts => {
+        const postFigure = document.createElement("Figure");
+        postFigure.classList.add("Post-box");
+        postFigure.innerHTML = `
+        <h2>${posts.username}</h2>
+        <p>${posts.content}</p>
+        <p class="timeStap">${posts.timestamp}</p>
+        `;
+feed.appendChild(postFigure)
+    })
+}
+posts.push({
+    username: "ahmed",
+    content: "giggity",
+    timestamp: "Now"
+});
+displayPosts();
