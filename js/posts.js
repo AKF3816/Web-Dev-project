e906b5ad14506f893db1323e3de98a31859ef952
AKF3816 <aa2308229@qu.edu.qa>
2026-03-21 22:59:33 +0300

    added create post function
    semi finished, requires username from local storage of username

--- js/posts.js
+++ js/posts.js
@@ -13,6 +13,53 @@ function displayPosts(){
 feed.appendChild(postFigure)
     })
 }
+const pstButton = document.getElementById("pstButton");
+pstButton.addEventListener("click",createPost)
+function createPost(){
+    const time = new Date().toLocaleDateString();
+    const pstInput = document.getElementById("postText");
+    const content = pstInput.value.trim();
+    if (content === ""){
+        return;
+    }
+posts.push({
+    username: String(profile.username),
+    content: content,
+    timestamp: time
+})
+pstInput.value="";
+displayPosts();
+}
+posts.push({
+    username: "ahmed",
+    content: "giggity",
+    timestamp: "Now"
+});
+posts.push({
+    username: "ahmed",
+    content: "giggity",
+    timestamp: "Now"
+});
+posts.push({
+    username: "ahmed",
+    content: "ujrnewbiouvbgnewiuabnj rvieaur nvbigqwsvd bqerbvqierenu vbuqrejen gviquwwb vireqjbnguiqeron eruger nerjgerjnviuerjubnerjnviurfejnvbqeriunv iqe qier nbqi  eunvqfvneqrir vqeirj nnqeroiu b qef qeibjnv qei neqroiu n   qeiuV ER ERO IQNE  UERQ QOEUI VBNQ ",
+    timestamp: "Now"
+});
+posts.push({
+    username: "ahmed",
+    content: "giggity",
+    timestamp: "Now"
+});
+posts.push({
+    username: "ahmed",
+    content: "giggity",
+    timestamp: "Now"
+});
+posts.push({
+    username: "ahmed",
+    content: "giggity",
+    timestamp: "Now"
+});
 posts.push({
     username: "ahmed",
     content: "giggity",
