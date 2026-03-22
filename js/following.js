fefa4a02a482ac9cbecceac01851b6ccf6965260
Eissa Alkathiri <ea2307530@qu.edu.qa>
2026-03-22 19:41:26 +0300

    add followers and followings js files.

---
+++ js/following.js
@@ -0,0 +1,27 @@
+const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+const viewedUser = JSON.parse(localStorage.getItem("viewedUser")) || currentUser;
+
+// redirect if not logged in
+if (!currentUser) {
+    window.location.href = "login.html";
+}
+
+document.getElementById("title").textContent = `${viewedUser.username}'s Following`;
+
+const users = JSON.parse(localStorage.getItem("users")) || [];
+const following = users.filter(user => viewedUser.following.includes(user.id));
+
+const list = document.getElementById("userList");
+following.forEach(user => {
+    const li = document.createElement("li");
+    li.innerHTML = `<a href="#" onclick="viewProfile('${user.username}')">${user.username}</a>`;
+    list.appendChild(li);
+});
+
+function viewProfile(username) {
+    const user = users.find(u => u.username === username);
+    if (user) {
+        localStorage.setItem("viewedUser", JSON.stringify(user));
+        window.location.href = "profile.html";
+    }
+}
